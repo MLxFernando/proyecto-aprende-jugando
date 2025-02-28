f2b7342142b61e2f5ee9a5e3b7f3923f7b646d62
MLxFernando <fernandomacas68@gmail.com>
2025-02-27 21:35:58 -0500

    Correccion de errores en sonidos

--- Assets/Scripts/RespuestaManager.cs
+++ Assets/Scripts/RespuestaManager.cs
@@ -27,12 +27,10 @@ public class RespuestaManager : MonoBehaviour
     {
         if (indiceSeleccionado == indiceCorrecto)
         {
-            Debug.Log("¡Correcto!");
             audioSource.PlayOneShot(sonidoCorrecto);
         }
         else
         {
-            Debug.Log("¡Incorrecto!");
             audioSource.PlayOneShot(sonidoError);
         }
     }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -16,15 +16,25 @@ public class UIManager : MonoBehaviour
 
     public void ShowPanel(GameObject panelToShow)
     {
+        StartCoroutine(SwitchPanelWithDelay(panelToShow, 2f));
+    }
+
+    private IEnumerator SwitchPanelWithDelay(GameObject panelToShow, float delay)
+    {
+        // Mantiene el panel actual visible por el tiempo de delay
+        yield return new WaitForSeconds(delay);
+
+        // Oculta todos los paneles
         mainPanel.SetActive(false);
         panelRegistro.SetActive(false);
         panelNiveles.SetActive(false);
-        
+
         foreach (var panel in panelesJuego)
         {
             panel.SetActive(false);
         }
 
+        // Muestra el nuevo panel después del delay
         panelToShow.SetActive(true);
     }
 }
